0ed8caa00bbd46bc54dda23328912e087d735104
AndresDev28 <andresdev28@hotmail.com>
2025-09-20 00:27:58 +0200

    fix: Correct project structure for production config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,7 @@
 const { parse } = require("pg-connection-string");
 
+console.log("✅ CARGANDO CONFIGURACIÓN DE BASE DE DATOS DE PRODUCCIÓN...");
+
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
