c101fccbbb3de74d50433cdbb76fe6a02560984e
AndresDev28 <andresdev28@hotmail.com>
2025-09-23 13:10:31 +0200

    feat(config): Add and configure Cloudinary upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default ({ env }) => ({
+  // 1. Configuración de JWT
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  // 2. Nueva configuración para Cloudinary
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
